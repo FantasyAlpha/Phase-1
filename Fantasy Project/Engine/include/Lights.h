0cb3d5e3188fd5e9fcdccc33aa0a75b830992a5e
kai <0ultrakai0@gmail.com>
2016-08-23 16:33:12 +0200

    reset

--- Fantasy Project/Engine/include/Lights.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Vertex.h>
-
-struct AmbientLight
-{
-	vec4f LightColor = vec4f{ 1, 1, 1, 1 };
-	float LightStrength = 0.9f;
-};
--- Fantasy Project/_Math/MathFunctions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <math.h>
-
-#define PI 3.14159265359f 
-
-#define ToRadians(a) (a * (PI / 180.0f))
-#define ArrayCount(arr) (sizeof(arr) / sizeof(arr[0]))
